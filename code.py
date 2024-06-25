2b99e332c8210a7460bce43be4c5f2eaa07ebfb1
rajayushkgp <164731167+rajayushkgp@users.noreply.github.com>
2024-06-25 19:53:01 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,236 @@
+import opendatasets as od
+dataset_url = 'https://www.kaggle.com/c/new-york-city-taxi-fare-prediction/overview'
+%%time
+od.download(dataset_url)
+!head {data_dir}/train.csv
+
+%%time
+selected_cols = 'fare_amount,pickup_datetime,pickup_longitude,pickup_latitude,dropoff_longitude,dropoff_latitude,passenger_count'.split(',')
+dtypes = {
+    'fare_amount': 'float32',
+    'pickup_longitude': 'float32',
+    'pickup_latitude': 'float32',
+    'dropoff_longitude': 'float32',
+    'passenger_count': 'float32'
+}
+
+def skip_row(row_idx):
+    if row_idx == 0:
+        return False
+    return random.random() > sample_frac
+
+random.seed(42)
+df = pd.read_csv(data_dir+"/train.csv",
+                 usecols=selected_cols,
+                 dtype=dtypes,
+                 parse_dates=['pickup_datetime'],
+                 skiprows=skip_row)
+
+df.head()
+test_df = pd.read_csv(data_dir+'/test.csv', dtype=dtypes, parse_dates=['pickup_datetime'])
+df.info()
+df.describe()
+df.pickup_datetime.min(), df.pickup_datetime.max()
+test_df.info()
+test_df.describe()
+test_df.pickup_datetime.min(), test_df.pickup_datetime.max()
+from sklearn.model_selection import train_test_split
+train_df, val_df = train_test_split(df, test_size=0.2, random_state=42)
+len(train_df), len(val_df)
+train_df = train_df.dropna()
+val_df = val_df.dropna()
+df.columns
+input_cols = ['pickup_longitude', 'pickup_latitude', 'dropoff_longitude', 'dropoff_latitude', 'passenger_count']
+target_col = 'fare_amount'
+train_inputs = train_df[input_cols]
+train_targets = train_df[target_col]
+train_inputs
+train_targets
+val_inputs = val_df[input_cols]
+val_targets = val_df[target_col]
+val_inputs
+val_targets
+test_inputs = test_df[input_cols]
+test_inputs
+#Adding Distance between Pickup and Drop locations using haversine formula
+import numpy as np
+
+def haversine_np(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees)
+
+    All args must be of equal length.
+
+    """
+    lon1, lat1, lon2, lat2 = map(np.radians, [lon1, lat1, lon2, lat2])
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = np.sin(dlat/2.0)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2.0)**2
+
+    c = 2 * np.arcsin(np.sqrt(a))
+    km = 6367 * c
+    return km
+ def add_trip_distance(df):
+    df['trip_distance'] = haversine_np(df['pickup_longitude'], df['pickup_latitude'], df['dropoff_longitude'], df['dropoff_latitude']) 
+add_trip_distance(train_df)
+add_trip_distance(val_df)
+add_trip_distance(test_df)
+train_df.sample(5)
+#Added distance from popular landmarks such as JFK Airport, LGA Airport, EWR Airport, Times Square , Met Meuseum, World Trade Center
+jfk_lonlat = -73.7781, 40.6413
+lga_lonlat = -73.8740, 40.7769
+ewr_lonlat = -74.1745, 40.6895
+met_lonlat = -73.9632, 40.7794
+wtc_lonlat = -74.0099, 40.7126
+def add_landmark_dropoff_distance(df, landmark_name, landmark_lonlat):
+    lon, lat = landmark_lonlat
+    df[landmark_name + '_drop_distance'] = haversine_np(lon, lat, df['dropoff_longitude'], df['dropoff_latitude'])
+%%time
+for a_df in [train_df, val_df, test_df]:
+    for name, lonlat in [('jfk', jfk_lonlat), ('lga', lga_lonlat), ('ewr', ewr_lonlat), ('met', met_lonlat), ('wtc', wtc_lonlat)]:
+        add_landmark_dropoff_distance(a_df, name, lonlat)
+
+train_df.sample(5)
+#Removing the outliers
+def remove_outliers(df):
+    return df[(df['fare_amount'] >= 1.) &
+              (df['fare_amount'] <= 500.) &
+              (df['pickup_longitude'] >= -75) &
+              (df['pickup_longitude'] <= -72) &
+              (df['dropoff_longitude'] >= -75) &
+              (df['dropoff_longitude'] <= -72) &
+              (df['pickup_latitude'] >= 40) &
+              (df['pickup_latitude'] <= 42) &
+              (df['dropoff_latitude'] >=40) &
+              (df['dropoff_latitude'] <= 42) &
+              (df['passenger_count'] >= 1) &
+              (df['passenger_count'] <= 6)]
+train_df = remove_outliers(train_df)
+val_df = remove_outliers(val_df)
+train_df.columns
+input_cols = ['pickup_longitude', 'pickup_latitude',
+       'dropoff_longitude', 'dropoff_latitude', 'passenger_count',
+       'pickup_datetime_year', 'pickup_datetime_month', 'pickup_datetime_day',
+       'pickup_datetime_weekday', 'pickup_datetime_hour', 'trip_distance',
+       'jfk_drop_distance', 'lga_drop_distance', 'ewr_drop_distance',
+       'met_drop_distance', 'wtc_drop_distance']
+target_col = 'fare_amount'
+train_inputs = train_df[input_cols]
+train_targets = train_df[target_col]
+val_inputs = val_df[input_cols]
+val_targets = val_df[target_col]
+test_inputs = test_df[input_cols]
+def evaluate(model):
+    train_preds = model.predict(train_inputs)
+    train_rmse = mean_squared_error(train_targets, train_preds, squared=False)
+    val_preds = model.predict(val_inputs)
+    val_rmse = mean_squared_error(val_targets, val_preds, squared=False)
+    return train_rmse, val_rmse, train_preds, val_preds
+def predict_and_submit(model, fname):
+    test_preds = model.predict(test_inputs)
+    sub_df = pd.read_csv(data_dir+'/sample_submission.csv')
+    sub_df['fare_amount'] = test_preds
+    sub_df.to_csv(fname, index=None)
+    return sub_df
+#Ridge Regression Model
+from sklearn.linear_model import Ridge
+model1 = Ridge(random_state=42)
+%%time
+model1.fit(train_inputs, train_targets)
+evaluate(model1)
+predict_and_submit(model1, 'ridge_submission.csv')
+
+
+#Random Forest Model
+from sklearn.ensemble import RandomForestRegressor
+model2 = RandomForestRegressor(max_depth=10, n_jobs=-1, random_state=42, n_estimators=50)
+%%time
+model2.fit(train_inputs, train_targets)
+evaluate(model2)
+predict_and_submit(model2, 'rf_submission.csv')
+
+#Gradient Boosting Model
+from xgboost import XGBRegressor
+model3 = XGBRegressor(random_state=42, n_jobs=-1, objective='reg:squarederror')
+%%time
+model3.fit(train_inputs, train_targets)
+evaluate(model3)
+predict_and_submit(model3, 'xgb_submission.csv')
+
+#Hyperparameter Tuning for better Accuracy
+import matplotlib.pyplot as plt
+
+def test_params(ModelClass, **params):
+    """Trains a model with the given parameters and returns training & validation RMSE"""
+    model = ModelClass(**params).fit(train_inputs, train_targets)
+    train_rmse = mean_squared_error(model.predict(train_inputs), train_targets, squared=False)
+    val_rmse = mean_squared_error(model.predict(val_inputs), val_targets, squared=False)
+    return train_rmse, val_rmse
+
+def test_param_and_plot(ModelClass, param_name, param_values, **other_params):
+    """Trains multiple models by varying the value of param_name according to param_values"""
+    train_errors, val_errors = [], []
+    for value in param_values:
+        params = dict(other_params)
+        params[param_name] = value
+        train_rmse, val_rmse = test_params(ModelClass, **params)
+        train_errors.append(train_rmse)
+        val_errors.append(val_rmse)
+
+    plt.figure(figsize=(10,6))
+    plt.title('Overfitting curve: ' + param_name)
+    plt.plot(param_values, train_errors, 'b-o')
+    plt.plot(param_values, val_errors, 'r-o')
+    plt.xlabel(param_name)
+    plt.ylabel('RMSE')
+    plt.legend(['Training', 'Validation'])
+
+best_params = {
+    'random_state': 42,
+    'n_jobs': -1,
+    'objective': 'reg:squarederror'
+}
+#No. of Trees
+%%time
+test_param_and_plot(XGBRegressor, 'n_estimators', [100, 250, 500], **best_params)
+best_params['n_estimators'] = 250
+#Max Depth
+%%time
+test_param_and_plot(XGBRegressor, 'max_depth', [3, 4, 5], **best_params)
+
+best_params['max_depth'] = 5
+%%time
+test_param_and_plot(XGBRegressor, 'learning_rate', [0.05, 0.1, 0.25], **best_params)
+
+best_params['learning_rate'] = 0.25
+
+xgb_model_final = XGBRegressor(objective='reg:squarederror', n_jobs=-1, random_state=42,
+                               n_estimators=500, max_depth=5, learning_rate=0.1,
+                               subsample=0.8, colsample_bytree=0.8)
+
+%%time
+xgb_model_final.fit(train_inputs, train_targets)
+evaluate(xgb_model_final)
+
+predict_and_submit(xgb_model_final, 'xgb_tuned_submission.csv')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
